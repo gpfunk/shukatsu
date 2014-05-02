9eccc4d7f2d6b7e2c836dcc08a87396e1af66434
gfunk <g.funk87@gmail.com>
2014-05-02 02:46:07 -0700

    Added installation instructions to Readme

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ shukatsu
 ========
 
 Application to manage and track your hunt for a job.
+
+Setup Instructions for Angular Front end.
+
+1. clone Repo
+2. cd into root of project
+3. grunt serve
+
+Should be it
+
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,31 @@
 'use strict';
 
 angular.module('shukatsuApp')
-  .controller('MainCtrl', function ($scope) {
-  	$scope.map = {
-  		center: {
-  			latitude: 45,
-  			longitude: -73
-  		},
-  		zoom: 8,
-  		options: {
-  			streetViewControl: false,
-  			panControl: false,
-  			mapTypeControl: false
-  		}
-  	};
-  });
+.controller('MainCtrl', function ($scope) {
+	// var pinColor = '2F76EE';
+
+	// var pinImage = new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + pinColor,
+	// 	new google.maps.Size(21, 34),
+	// 	new google.maps.Point(0,0),
+	// 	new google.maps.Point(10, 34));
+
+	$scope.map = {
+		center: {
+			latitude: 45,
+			longitude: -73
+		},
+		zoom: 8,
+		options: {
+			streetViewControl: false,
+			panControl: false,
+			mapTypeControl: false
+		}
+	};
+
+	$scope.marker = {
+		coords: {
+			latitude: -34.397,
+			longitude: 150.644
+		}
+	}
+});
